069e8727061f04fdc11a606ed40725a19e1c1f71
Ethan Bielecki <ebielecki2003@gmail.com>
2024-05-19 21:49:26 -0400

    Added some styling to the login and forgot password screens

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import '../CSS/Base.css'
+import '../CSS/login.css'
 
 export const Login = () => {
     const [formData, setFormData] = useState({
@@ -15,18 +17,20 @@ export const Login = () => {
     }
 
     const handleFormSubmit = () => {
-
+        window.href='#'
     }
 
     return (
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <input type="text" className="form-control" placeholder="Username" id="username" name="username" onChange ={handleInputChange} value={FormData.username}/>
-                <input type="password" className="form-control" placeholder="Password" id="password" name="" onChange ={handleInputChange} value={FormData.username}/>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-
-            <button type="button" className="btn btn-link">Forgot Password</button>
+        <div className="page-wrapper">
+            <div className="login-container">
+                <h4> <center> Please enter your login information </center> </h4>
+                <form onSubmit={handleFormSubmit}>
+                    <input type="text" className="form-control username-text" placeholder="Username" id="username" name="username" onChange ={handleInputChange} value={FormData.username}/>
+                    <input type="password" className="form-control password-text" placeholder="Password" id="password" name="" onChange ={handleInputChange} value={FormData.username}/>
+                    <a href="#/ResetPassword"> <button type="button" className="btn btn-link login-forgot-password-color">Forgot Password</button> </a>
+                    <button type="submit" className="btn btn-primary button-justify-right">Login</button>
+                </form>
+            </div>
         </div>
     )
 }
--- Frontend/src/Pages/ResetPassword.jsx
+++ Frontend/src/Pages/ResetPassword.jsx
@@ -1,6 +1,34 @@
+import React, {useState} from 'react'
+import '../CSS/Base.css'
+import '../CSS/ResetPassword.css'
+
 export const ResetPassword = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setFormData({
+            ...formdata,
+            [event.target.name]: value,
+        })
+    }
+
+    const handleFormSubmit = () => {
+
+    }
+
     return (
-        <>
-        </>
+        <div className='page-wrapper'>
+            <div className="reset-password-container">
+                <h4> <center> Reset your password </center> </h4>
+                <form onSubmit={handleFormSubmit}>
+                    <input type="text" className="form-control email-text" placeholder="Email" id="email" name="email" onChange ={handleInputChange} value={FormData.username}/>
+                    <button type="submit" className="btn btn-primary button-justify-right">Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
